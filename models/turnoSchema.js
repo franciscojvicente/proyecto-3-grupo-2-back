e1910b8bccc717698f69de3b63d09a67cfac69ff
franv <fran.vicente03@gmail.com>
2023-09-29 16:53:43 -0300

    add userSchema

--- models/turnoSchema.js
+++ models/turnoSchema.js
@@ -3,23 +3,28 @@ const mongoose = require("mongoose");
 const turnoSchema = new mongoose.Schema({
     vet: {
         type: String,
-        require: true
+        require: true,
+        trim: true
     },
     pet: {
         type: String,
-        require: true
+        require: true,
+        trim: true
     },
     date: {
         type: Date,
-        require: true
+        require: true,
+        trim: true
     },
     hour: {
         type: Number,
-        require: true
+        require: true,
+        trim: true
     },
     details: {
         type: String,
-        require: true
+        require: true,
+        trim: true
     }
 })
 
--- models/userSchema.js
+++ models/userSchema.js
@@ -1,1 +1,17 @@
-// archivo del user Schema
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        require: true,
+        trim: [true, "No puede tener espacios"]
+    }
+})
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
