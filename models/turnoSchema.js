4a54fd68ec9ad57f39d16cc33beaafb292f0ed18
franv <fran.vicente03@gmail.com>
2023-10-03 12:50:33 -0300

    agrego modificacion de la fecha al schema turno

--- models/turnoSchema.js
+++ models/turnoSchema.js
@@ -12,9 +12,29 @@ const turnoSchema = new mongoose.Schema({
         trim: true
     },
     date: {
-        type: Date,
-        require: true,
-        trim: true
+    type: String, // Almacenaremos la fecha como una cadena en el formato 'yyyy-mm-dd'
+    set: function (date) {
+      // La función set se ejecutará cuando se establezca el valor del atributo 'date'
+      if (date instanceof Date) {
+        // Si la entrada es una instancia de Date, la formateamos como 'yyyy-mm-dd'
+        return date.toISOString().split('T')[0];
+      } else if (typeof date === 'string') {
+        // Si la entrada es una cadena, intentamos convertirla al formato 'yyyy-mm-dd'
+        const parts = date.split('-');
+        if (parts.length === 3) {
+          return `${parts[2]}-${parts[1]}-${parts[0]}`;
+        }
+      }
+      // Si no podemos convertir la fecha, la dejamos como está
+      return date;
+    },
+    get: function (date) {
+      // La función get se ejecutará cuando obtengas el valor del atributo 'date'
+      if (date) {
+        // Si hay una fecha válida, la retornamos
+        return date;
+      }
+    },
     },
     hour: {
         type: Number,
