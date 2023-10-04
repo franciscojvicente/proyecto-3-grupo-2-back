ea70162d2fa5fc6a80ae88f3efc181b282ed617d
franv <fran.vicente03@gmail.com>
2023-10-04 19:03:21 -0300

    creo template para el envio de formulario

---
+++ utils/template.js
@@ -0,0 +1,29 @@
+const template = (newDetail) => {
+    return (
+    `
+    <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+            <style>
+                body {
+                    background-color: aquamarine;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>¡Hola, ${newDetail.name}!</h1>
+            <h3>Recibimos tu solicitud sobre el plan ${newDetail.plan}, en el que nos manifestaste lo siguiente:</h3>
+            <p>${newDetail.details}</p>
+            <h3>Pronto nos pondremos en contacto con el número que nos aportaste (${newDetail.cellphone}), en caso de ser incorrecto, te pedimos que llenes nuevamente el formulario.</h3>
+            <h5>Atentamente: Veterinaria Patitas. ¡Saludos! </h5>
+            
+        </body>
+        </html>
+    `
+    )
+}
+
+module.exports = template;
