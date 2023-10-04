5b2db9ffaa4400226735a81d502f3a76d1e15943
franv <fran.vicente03@gmail.com>
2023-10-04 18:30:14 -0300

    agrego plan al detail controller

--- controllers/detailController.js
+++ controllers/detailController.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const sendEmail = require("../utils/emailHandler");
 
 const newRequest = async (req, res) => {
-    const { name, lastname, email, cellphone, details } = req.body;
+    const { name, lastname, email, cellphone, plan, details } = req.body;
     const detail = await Detail.findOne({email, cellphone});
     try {
         if(detail) {
