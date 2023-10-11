9975c3872c938f81ba5a94bfb27499df403a11db
franv <fran.vicente03@gmail.com>
2023-10-11 17:01:54 -0300

    modifico date en turnoSchema

--- models/turnoSchema.js
+++ models/turnoSchema.js
@@ -13,23 +13,27 @@ const turnoSchema = new mongoose.Schema({
         trim: true
     },
     date: {
-        type: Date,
-        required: true,
-        get: function (date) {
-            if (date instanceof Date) {
-                const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
-                return date.toLocaleDateString('es-AR', options);
-            }
-            return date;
-        },
-        set: function (date) {
-            if (typeof date === 'string') {
-                const [day, month, year] = date.split('/').map(Number);
-                return new Date(year, month - 1, day);
-            }
-            return date;
+    type: Date,
+    required: true,
+    get: function (date) {
+        if (date instanceof Date) {
+            const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
+            return date.toLocaleDateString('es-AR', options);
         }
+        return date;
     },
+    set: function (date) {
+        if (typeof date === 'string') {
+            const [day, month, year] = date.split('/').map(Number);
+            // Verificar si la fecha es válida
+            if (isNaN(day) || isNaN(month) || isNaN(year)) {
+                return date; // Devuelve la fecha original si no es válida
+            }
+            return new Date(year, month - 1, day);
+        }
+        return date;
+    }
+},
     hour: {
         type: String,
         required: true,
